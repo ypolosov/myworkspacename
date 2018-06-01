3dda0b623d4133a7e9f3edefba343a889d79c929
ypolosov <ypolosov@gmail.com>
2018-06-01 11:32:51 +0300

    init repo

---
+++ libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
+
+export const uiRoutes: Route[] = [];
+@NgModule({
+  imports: [CommonModule, RouterModule]
+})
+export class UiModule {
+  constructor() {
+    console.log('xxx');
+  }
+}
